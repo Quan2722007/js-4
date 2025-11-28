61105b08392a9ae8c83e49219257832b07e0ce39
Ngô Văn Quân <25648131.quan@student.iuh.edu.vn>
2025-11-28 19:38:31 +0700

    first commit

---
+++ style.js
@@ -0,0 +1,61 @@
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const result = document.getElementById("result");
+
+document.getElementById("input1").addEventListener("input", function () {
+    this.value = this.value.replace(/[^0-9.-]/g, "");
+});
+document.getElementById("input2").addEventListener("input", function () {
+    this.value = this.value.replace(/[^0-9.-]/g, "");
+});
+
+function calculator(operation) {
+    result.className = "";
+
+    if (input1.value === "" || input2.value === "") {
+        result.innerHTML = "Hãy nhập số vào đủ";
+        return;
+    }
+
+    let numb1 = parseFloat(input1.value);
+    let numb2 = parseFloat(input2.value);
+    if (isNaN(numb1) || isNaN(numb2)) {
+        result.innerHTML = "Lỗi dữ liệu";
+        return;
+    }
+
+    let output;
+
+    switch (operation) {
+        case "getAdd":
+            output = numb1 + numb2;
+            break;
+        case "getSub":
+            output = numb1 - numb2;
+            break;
+        case "getMul":
+            output = numb1 * numb2;
+            break;
+        case "getDiv":
+            if (numb2 === 0) {
+                output = "Không có số nào chia cho 0";
+                break;
+            } else {
+                output = numb1 / numb2;
+                break;
+            }
+    }
+    result.innerHTML = output;
+}
+document
+    .getElementById("getAdd")
+    .addEventListener("click", () => calculator("getAdd"));
+document
+    .getElementById("getSub")
+    .addEventListener("click", () => calculator("getSub"));
+document
+    .getElementById("getMul")
+    .addEventListener("click", () => calculator("getMul"));
+document
+    .getElementById("getDiv")
+    .addEventListener("click", () => calculator("getDiv"));
